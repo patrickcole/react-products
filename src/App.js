58d23c6f04f476d0f4d0510af1ea2a621be6b6e4
Patrick Cole <hpcmobiledev@gmail.com>
2019-02-04 20:59:49 -0500

    Provides initial product view with controls to update quantity
    * Built using react-hooks for state-management.
    * Purely functional components.
    * Quantity updates the total based on the price.
    * Checks for numeric characters only in the quantity field.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,117 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function Price({ title, amount }) {
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{amount}</p>
+    </div>
+  )
+}
+
+function Quantity({ amount, addQuantity, removeQuantity, updateQuantity }) {
+
+  const quantityChanged = (e) => {
+    updateQuantity(e.target.value);
   }
+
+  return (
+    <div>
+      <h3>Quantity</h3>
+      <input type="text" onChange={quantityChanged} value={ amount } />
+      <button onClick={addQuantity}>+</button>
+      <button onClick={removeQuantity}>-</button>
+    </div>
+  )
+}
+
+function Product( { item }) {
+
+  const [product, setProduct] = useState(item);
+
+  const isNumeric = (n) => {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  const calculateTotal = () => {
+    return product.quantity * product.price;
+  };
+
+  const addQuantity = () => {
+    const newProduct = {
+      ...product,
+      quantity: product.quantity + 1
+    };
+    setProduct(newProduct);
+  };
+
+  const removeQuantity = () => {
+    if ( product.quantity > 0 ){
+      const newProduct = {
+        ...product,
+        quantity: product.quantity - 1
+      };
+      setProduct(newProduct);
+    }
+  };
+
+  const updateQuantity = (val) => {
+
+    let newValue = 0;
+    if ( isNumeric(val) ) {
+      newValue = parseInt(val);
+    }
+
+    const newProduct = {
+      ...product,
+      quantity: newValue
+    };
+    setProduct(newProduct);
+  }
+
+  return (
+    <div className="product">
+      <h3>{ product.title} </h3>
+      <p>{ product.description }</p>
+      <Price title="Price" amount={product.price} />
+      <Quantity amount={product.quantity} addQuantity={addQuantity} removeQuantity={removeQuantity} updateQuantity={updateQuantity} />
+      <Price title="Total" amount={calculateTotal()} />
+    </div>
+  )
+}
+
+function App() {
+
+  const productData = [
+    {
+      title: 'React Book',
+      description: 'Ready to start using React?',
+      quantity: 1,
+      price: 10
+    },
+    {
+      title: 'Angular Book',
+      description: 'From Google!',
+      quantity: 4,
+      price: 25
+    }
+  ];
+
+  const [products, setProducts] = useState(productData);
+
+  return (
+    <main className="cart">
+      <h1>React Product</h1>
+      <p>Having fun playing with React Hooks while building this product/cart form.</p>
+      <div className="products">
+        {products.map( (product,index) => (
+          <Product key={index} item={product} />
+        ))}
+      </div>
+    </main>
+  )
 }
 
 export default App;
